e2498a3b536507c3e6cd4022d8d01cc5a92a3270
phlima3 <psantoslima86@gmail.com>
2025-05-19 17:06:39 -0300

    chore: remove Vercel Analytics integration and update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/next";
 // Import i18n (needs to happen before any components that use translations)
 import "./i18n";
 
@@ -40,7 +39,6 @@ const App = () => (
           <TooltipProvider>
             <Toaster />
             <Sonner />
-            <Analytics />
             <BrowserRouter>
               <div className="min-h-screen flex flex-col">
                 <Navbar />
