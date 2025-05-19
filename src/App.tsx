2db0162f01b498a6e3ad6054c3a8ed94291b0ef8
phlima3 <psantoslima86@gmail.com>
2025-05-19 16:57:35 -0300

    feat: add Vercel Analytics integration and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { Analytics } from "@vercel/analytics/next";
 // Import i18n (needs to happen before any components that use translations)
 import "./i18n";
 
@@ -40,6 +40,7 @@ const App = () => (
           <TooltipProvider>
             <Toaster />
             <Sonner />
+            <Analytics />
             <BrowserRouter>
               <div className="min-h-screen flex flex-col">
                 <Navbar />
